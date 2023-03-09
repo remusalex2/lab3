da3b86d6e219506d10c557b74872f23c847fcf26
remusalex2 <remus.alex2@gmail.com>
2023-03-09 11:46:34 +0200

    Initial commit

---
+++ src/lab3/lab3.java
@@ -0,0 +1,25 @@
+package lab3;
+
+class Parabola {
+    private int a;
+    private int b;
+    private int c;
+
+    public Parabola(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    @Override
+    public String toString() {
+        return "f(x) = a * x^2 + b * x + c";
+    }
+
+    public int Calcul(Parabola p)
+    }
+        //return x = (x1 + x2) / 2;
+        return 0;
+    }
+}
